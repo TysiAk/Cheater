514a1bd9d9423f8ef75b224f6baa9fe5427fb949
TysiAk <33802198+TysiAk@users.noreply.github.com>
2017-11-19 13:01:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ myUrl = str(input('Type URL link: '))
 headers = {}
 headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
 
-#Request UTL using fake useragents
+#Request URL using fake useragents
 req = urllib.request.Request(myUrl, headers = headers)
 resp = urllib.request.urlopen(req)
 
